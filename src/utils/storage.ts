d0a1763284cf869614b113559eeb68952e394a53
ifcheng <fchengmail@163.com>
2020-06-30 10:58:20 +0800

    localStorage可设置过期时间

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,17 +1,58 @@
+function stringToDate(str: string): Date {
+  const matchs = str.match(/^\d+(\.\d+)?([hdy])$/)
+  if (matchs) {
+    const now = Date.now()
+    const maxAge = parseFloat(str)
+    return matchs[2] === 'h'
+      ? new Date(now + maxAge * 3600 * 1000)
+      : matchs[2] === 'd'
+      ? new Date(now + maxAge * 24 * 3600 * 1000)
+      : new Date(now + maxAge * 365 * 24 * 3600 * 1000)
+  }
+  const date = new Date(str)
+  return isNaN(date.getTime()) ? new Date(0) : date
+}
+
+interface Ref<T = any> {
+  value: T
+  expires?: string
+}
+
+type Expires = number | string | Date
+
+function ref<T = any>(value: T, expires?: Expires): Ref<T> {
+  let sExpires: Date | undefined
+  if (typeof expires === 'number') {
+    sExpires = new Date(Date.now() + expires * 1000)
+  } else if (typeof expires === 'string') {
+    sExpires = stringToDate(expires)
+  } else if (expires instanceof Date) {
+    sExpires = expires
+  }
+  return {
+    value,
+    ...(sExpires && { expires: sExpires.toUTCString() }),
+  }
+}
+
 class SmartStorage {
   constructor(public native: Storage) {}
   get<R>(key: string): R | null {
     let res = this.native.getItem(key)
     try {
-      res = JSON.parse(res as string).value
+      const { value, expires } = JSON.parse(res as string) as Ref
+      if (expires && new Date(expires).getTime() <= Date.now()) {
+        this.remove(key)
+        res = null
+      } else {
+        res = value
+      }
     } catch {}
     return res as any
   }
-  set(key: string, value: any): void {
-    if (typeof value !== 'string') {
-      value = JSON.stringify({ value })
-    }
-    this.native.setItem(key, value)
+  set(key: string, value: any, expires?: Expires): void {
+    expires = this.native === localStorage ? expires : void 0
+    this.native.setItem(key, JSON.stringify(ref(value, expires)))
   }
   remove(key: string | string[]): void {
     if (Array.isArray(key)) {
